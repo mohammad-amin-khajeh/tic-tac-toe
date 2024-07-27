1d81a39a74c488130de16f9cb077498ff4ae63ba
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-07-27 11:25:28 +0330

    Format currentStatus according to biome rules

--- src/board.tsx
+++ src/board.tsx
@@ -5,7 +5,11 @@ import { TrackStatus, CurrentStatus } from "./status";
 export default function Board({ xIsNext, squares, onPlay, moveNumber }) {
 	// tracking the status of the game
 	const potentialWinner = CalculateWinner(squares);
-	const currentStatus: string = CurrentStatus(potentialWinner, xIsNext, moveNumber);
+	const currentStatus: string = CurrentStatus(
+		potentialWinner,
+		xIsNext,
+		moveNumber,
+	);
 
 	const handleClick = (index: number) => {
 		if (squares[index] || CalculateWinner(squares)) {
